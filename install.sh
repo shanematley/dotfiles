ceb76ebca35b1ff863c19bd1ff86b53c775cb7d8
Shane <general@matley.com.au>
2014-04-26 12:50:24 +0100

    Added basic install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Installation script creates links in the user's home directory
+
+FILES=("vimrc" "vim" "tmux" "tmux.conf")
+cat <<EOF
+This script will create soft links to the following files in the user's home
+directory:
+
+    ${FILES[@]}
+
+EOF
+
+while true; do
+    read -p "Would you like to continue [Y/N]? " answer
+    case $answer in
+        [yY]*) echo ""; break;;
+        [nN]*) echo "Aborting"; exit;;
+    esac
+done
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+function create_link {
+    local DEST="$HOME/.$1"
+    local SRC="$SCRIPTPATH/$1"
+    [[ -e "$DEST" && ! -L "$DEST" ]] && { echo "ERROR: $DEST already exists. Skipping."; return 1; }
+    echo "Linking $DEST -> $SRC"
+    ln -sf "$SRC" "$DEST"
+}
+
+for f in "${FILES[@]}"; do
+    create_link "$f"
+done
+
+echo
+echo "Vim and Tmux configuration now in place. To setup Vundle run: ./install_vundle.sh"
