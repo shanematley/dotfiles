64e642f10f40c8b782a1eb01d4704eaf8e0e94f6
Shane Matley <shane@matley.com.au>
2014-05-30 11:07:16 -0400

    Correct colours for Solaris

--- shrc.d/10prompt.zsh
+++ shrc.d/10prompt.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
+eval PR_NO_COLOUR='${terminfo[sgr0]%}'
 read -r -d '' PROMPT <<EOF
 $PR_SET_CHARSET$PR_STITLE${(e)PR_TITLEBAR}\
 [\
 $PR_GREEN%n@%m$PR_NO_COLOUR:\
-$PR_CYAN%~$PR_GREEN$PR_BLUE\
+$PR_CYAN%~$PR_NO_COLOUR\
 ]%(!.$PR_RED.$PR_NO_COLOUR)%#
 EOF
 PROMPT="${PROMPT} "
