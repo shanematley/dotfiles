659f03a0e5fad5fc580e6d97fb8d29266f98fa26
Shane Matley <shane@matley.com.au>
2014-06-09 11:42:58 -0400

    ZSH: Embed escape sequences in percent-braces construct

--- shrc.d/10prompt.zsh
+++ shrc.d/10prompt.zsh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
+# For additional information on the options available, refer to
+# section EXPANSION OF PROMPT SEQUENCES of zshmisc(1)
 eval PR_NO_COLOUR='${terminfo[sgr0]%}'
 read -r -d '' PROMPT <<EOF
 $PR_SET_CHARSET$PR_STITLE${(e)PR_TITLEBAR}\
 [\
-$PR_GREEN%n@%m$PR_NO_COLOUR:\
-$PR_CYAN%~$PR_NO_COLOUR\
-]%(!.$PR_RED.$PR_NO_COLOUR)%#
+%{$PR_GREEN%}%n@%m%{$PR_NO_COLOUR%}:\
+%{$PR_CYAN%}%~%{$PR_NO_COLOUR%}\
+]%{%(!.$PR_RED.$PR_NO_COLOUR)%}%#
 EOF
 PROMPT="${PROMPT} "
 
