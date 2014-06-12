5e5af1847fa899c071e1760178a8512f41eebe6b
Shane <general@matley.com.au>
2014-06-12 11:50:26 -0400

    ZSH: Fix color prompt on Solaris

--- shrc.d/10prompt.zsh
+++ shrc.d/10prompt.zsh
@@ -2,13 +2,12 @@
 
 # For additional information on the options available, refer to
 # section EXPANSION OF PROMPT SEQUENCES of zshmisc(1)
-eval PR_NO_COLOUR='${terminfo[sgr0]%}'
 read -r -d '' PROMPT <<EOF
 $PR_SET_CHARSET$PR_STITLE${(e)PR_TITLEBAR}\
 [\
-%{$PR_GREEN%}%n@%m%{$PR_NO_COLOUR%}:\
-%{$PR_CYAN%}%~%{$PR_NO_COLOUR%}\
-]%{%(!.$PR_RED.$PR_NO_COLOUR)%}%#
+%{$PR_GREEN%}%n@%m%{$reset_color%}:\
+%{$PR_CYAN%}%~%{$reset_color%}\
+]%{%(!.$PR_RED.$reset_color)%}%#
 EOF
 PROMPT="${PROMPT} "
 
