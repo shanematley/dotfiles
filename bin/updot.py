1af379132e25d6dd8b0a0455da515e2a27612aee
Shane Matley <shane@matley.com.au>
2015-01-05 12:46:59 -0500

    BIN: updot compatibility with older git + -n and --create flags added

--- bin/updot.py
+++ bin/updot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import subprocess
 import yaml
 import os
@@ -23,9 +24,9 @@ def update_repository(folder, host=None):
     UPDATE_SCRIPT="""cd {remote_loc}
     printf "  Repo [$(pwd)]: "
     git fetch -q
-    if [[ $(git rev-parse @) != $(git rev-parse @{{u}}) ]]; then
-        [[ $(git rev-parse @) == $(git merge-base @ @{{u}}) ]] && {{ echo "Out of date"; git pull --rebase; }}
-        [[ $(git rev-parse @) != $(git rev-parse @{{u}}) ]] && echo -e "\\nNOTE: The repository on {host} has unpushed changes!"
+    if [[ $(git rev-parse HEAD) != $(git rev-parse HEAD@{{u}}) ]]; then
+        [[ $(git rev-parse HEAD) == $(git merge-base HEAD HEAD@{{u}}) ]] && {{ echo "Out of date"; git pull --rebase; }}
+        [[ $(git rev-parse HEAD) != $(git rev-parse HEAD@{{u}}) ]] && echo -e "\\nNOTE: The repository on {host} has unpushed changes!"
     else
         echo "Up to date"
     fi
@@ -37,10 +38,30 @@ def update_repository(folder, host=None):
         subprocess.call(UPDATE_SCRIPT.format(**{'host':'this host', 'remote_loc':folder}), shell=True)
     print
 
+def create_config_file():
+    try:
+        fd = os.open(os.path.expanduser(UPDOT_YAML_PATH), os.O_WRONLY | os.O_CREAT | os.O_EXCL)
+        with os.fdopen(fd, 'w') as f:
+            yaml.dump({'remotes' : []}, f, default_flow_style=False)
+    except OSError as e:
+        pass
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Update dotfiles")
+    parser.add_argument('--create', action='store_true', help='Create a new yaml config file')
+    parser.add_argument('--dry-run', '-n', action='store_true', help='Just print which hosts would be accessed.')
+    args = parser.parse_args()
+    if args.create:
+        create_config_file()
+        sys.exit(0)
+
+
     remote_locations = load_yaml()
     if not remote_locations:
         print "No hosts to update found in the configuration file", UPDOT_YAML_PATH
     for remote in remote_locations:
-        update_repository(**remote)
+        if args.dry_run:
+            print " *", remote['host'], ':', remote['folder']
+        else:
+            update_repository(**remote)
 
