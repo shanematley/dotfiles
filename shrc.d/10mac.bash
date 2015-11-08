abb288f990dde3093eeb99a1c1657a50e7327042
Shane <general@matley.com.au>
2015-11-08 15:27:44 +1100

    Add 10mac.bash with cdr_to_iso

---
+++ shrc.d/10mac.bash
@@ -0,0 +1,11 @@
+
+function cdr_to_iso() {
+    [[ -z $1 ]] &&  { echo "Usage: cdr_to_iso disk.cdr"; return 1; }
+    if [[ $1 =~ (.*)\.cdr$ ]]; then
+        echo "Creating ISO: $1 -> ${BASH_REMATCH[1]}.iso"
+        hdiutil makehybrid -iso -joliet -o ${BASH_REMATCH[1]}.iso $1
+    else
+         echo "Usage: cdr_to_iso disk.iso"
+         return 1
+     fi
+}
