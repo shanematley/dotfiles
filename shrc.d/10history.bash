ebe7961c22fe4661c2770c0be542302d7f3b09f1
Shane <general@matley.com.au>
2016-02-23 09:14:29 +1100

    BASH: Add persistent history

--- shrc.d/10history.bash
+++ shrc.d/10history.bash
@@ -7,3 +7,30 @@ HISTTIMEFORMAT='%F %T '
 
 # Rollup multi-line commands into a single line
 shopt -s cmdhist
+
+log_bash_persistent_history()
+{
+  [[
+    $(history 1) =~ ^\ *[0-9]+\ +([0-9-]+\ [0-9:]+)\ +(.*)$
+  ]]
+  local date_part="${BASH_REMATCH[1]}"
+  local command_part="${BASH_REMATCH[2]}"
+  if [ "$command_part" != "$PERSISTENT_HISTORY_LAST" ]
+  then
+    echo $date_part "|" "$command_part" >> ~/.persistent_history
+    export PERSISTENT_HISTORY_LAST="$command_part"
+  fi
+}
+
+run_on_prompt_command()
+{
+    log_bash_persistent_history
+}
+
+PROMPT_COMMAND="run_on_prompt_command"
+
+alias phgrep='cat ~/.persistent_history|grep --color'
+alias hgrep='history|grep --color'
+
+alias trim_persistant_history='tail -20000 ~/.persistent_history | tee ~/.persistent_history'
+
