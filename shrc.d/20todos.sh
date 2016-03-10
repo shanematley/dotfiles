4e9218a3cb9d62e8282712c712de472edb8f135b
Shane <general@matley.com.au>
2016-03-10 11:00:59 +1100

    SH: Add branch_todos

---
+++ shrc.d/20todos.sh
@@ -0,0 +1,22 @@
+function branch_todos()
+{
+    if [[ -z $1 ]]; then
+        cat<<EOF
+USAGE
+        branch_todos base_branch [branch_to_check]
+
+DESCRIPTION
+        List files changed in a branch containing TODO.
+
+EXAMPLES
+
+        Check TODOs in files changed between origin/master and HEAD:
+            branch_todos origin/master
+        
+        Check TODOs in files changed in master:
+            branch_todos origin/master master
+EOF
+        return 1
+    fi
+    grep TODO $(git diff $1...$2 --name-only) | sed 's/:.*//' | uniq -c
+}
