a73681daad5853231b270c9e3ca2af0a3bf074b8
Shane Matley <shane@matley.com.au>
2016-06-07 11:55:40 +1000

    ZSH: bind delete, home and end on Mac

--- shrc.d/30bindings.zsh
+++ shrc.d/30bindings.zsh
@@ -2,3 +2,12 @@ bindkey -e
 bindkey '^]'   'vi-find-next-char'
 bindkey '\M^]' 'vi-find-prev-char'
 bindkey '^U'  'backward-kill-line'
+
+# To determine what keys these actually are:
+# echo '<Ctrl-V><key>' | od -c
+# Where <Ctrl-V> is actually Ctrl-V and <key> is the key to test.
+if [[ $(uname) = Darwin ]]; then
+    bindkey '^[[3~' delete-char
+    bindkey '^[[1~' beginning-of-line
+    bindkey '^[[4~' end-of-line
+fi
