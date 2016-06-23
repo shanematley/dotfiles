ae949b8c0f9259a109874ea827c4b99965bac4c9
Shane Matley <shane@matley.com.au>
2016-06-23 16:42:26 +1000

    ZSH: Add history

--- shrc.d/10history.zsh
+++ shrc.d/10history.zsh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+export HISTFILE=~/.zsh_history
+export HISTSIZE=1024
+export SAVEHIST=1024
+
+setopt append_history
 setopt hist_ignore_space
 setopt hist_ignore_dups
+setopt share_history
+setopt hist_verify
+setopt inc_append_history
+
 alias fg=" fg"
