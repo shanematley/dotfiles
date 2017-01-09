b000ffb022df1da5de0d7edd5b753cc1b31c2fc3
Shane Matley <shane@matley.com.au>
2017-01-10 10:48:51 +1100

    SH: Adjust gs a little more

--- shrc.d/10git.sh
+++ shrc.d/10git.sh
@@ -2,8 +2,8 @@ function gs() {
     echo "$(tput bold)$(tput setaf 4)GIT STATUS$(tput sgr0)"
     git -c color.status=always status -sb|sed 's/^/  /'
     echo "$(tput bold)$(tput setaf 4)OUTGOING$(tput sgr0)"
-    git outgoing|sed 's/^/  /'
+    git -c color.status=always outgoing|sed 's/^/  /'
     echo "$(tput bold)$(tput setaf 4)RECENT$(tput sgr0)"
-    git --no-pager graph -10|sed 's/^/  /'
+    git --no-pager graph -10
 }
 alias gitn="git --no-pager"
