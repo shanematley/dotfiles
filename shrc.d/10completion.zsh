fb01e063137c7aedd559e15d5ee8eea6921eb215
Shane <general@matley.com.au>
2018-02-19 17:15:13 +1100

    ZSH: Cleanup zshcompletion

--- shrc.d/10completion.zsh
+++
@@ -1,6 +0,0 @@
-# Completions
-autoload -U compinit
-
-# case-insensitive (all),partial-word and then substring completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
-    'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
