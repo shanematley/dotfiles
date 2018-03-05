1daeeb40ad804ad0a59fc6643423fff5a848a3dc
Shane <general@matley.com.au>
2018-03-05 11:25:25 +1100

    ZSH: Fix broken key bindings

--- shrc.d/30bindings.zsh
+++ shrc.d/30bindings.zsh
@@ -3,6 +3,21 @@ bindkey '^]'   'vi-find-next-char'
 bindkey '\M^]' 'vi-find-prev-char'
 bindkey '^U'  'backward-kill-line'
 
+# Enable Ctrl-x-e to edit command line
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^xe' edit-command-line
+bindkey '^x^e' edit-command-line
+
+# Bash style word selection (e.g. CTRL-W and M-left)
+autoload -U select-word-style
+select-word-style bash
+
+# context help.
+autoload -Uz run-help
+autoload -Uz run-help-git
+bindkey "^[OP" run-help # F1 key
+
 # To determine what keys these actually are:
 # echo '<Ctrl-V><key>' | od -c
 # Where <Ctrl-V> is actually Ctrl-V and <key> is the key to test.
