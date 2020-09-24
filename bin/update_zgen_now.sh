046a36bcf3ed1c6c1733233b551abec656be4200
Shane <general@matley.com.au>
2020-09-24 13:51:19 +1000

    Update update_zgen_now.sh to work on Mac

--- bin/update_zgen_now.sh
+++ bin/update_zgen_now.sh
@@ -1,11 +1,24 @@
 #!/bin/zsh
 
-[[ ! -f ~/.zgen/init.zsh ]] && { echo "No ~/.zgen/init.zsh file. Can't update"; exit 1 }
-
-zsh -c '
 echo "Updating zgen $(date)"
-source ~/.zshrc
-ZGEN_PLUGIN_UPDATE_DAYS=0 ZGEN_SYSTEM_UPDATE_DAYS=0 zgen load unixorn/autoupdate-zgen
-zgen save
-'
 
+if [ -z "${ZGEN_SYSTEM_RECEIPT_F}" ]; then
+    ZGEN_SYSTEM_RECEIPT_F='.zgen_system_lastupdate'
+fi
+
+if [ -z "${ZGEN_PLUGIN_RECEIPT_F}" ]; then
+    ZGEN_PLUGIN_RECEIPT_F='.zgen_plugin_lastupdate'
+fi
+
+source ~/.shrc.d/00zgen/zgen.zsh
+#zgen reset
+load_cmds=$(grep -h -r --include="*.zsh" "^ *zgen load.*" ~/.shrc.d/* ~/.shrc.d.local/* | awk '{$1=$1};1')
+while read -r load_cmd; do
+    eval $load_cmd
+done <<< "$load_cmds"
+zgen update
+zgen save
+zgen list
+echo "Writing date to ${ZGEN_SYSTEM_RECEIPT_F}"
+date +%s >! ~/${ZGEN_SYSTEM_RECEIPT_F}
+date +%s >! ~/${ZGEN_PLUGIN_RECEIPT_F}
