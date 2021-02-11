8c4365ad8a27dcf37d8cdea309b711cee9c12cf2
Shane <general@matley.com.au>
2021-02-11 18:47:58 +1100

    Tweak Less

--- shrc.d/10less.sh
+++ shrc.d/10less.sh
@@ -3,12 +3,13 @@
 export PAGER=less
 export LESSCHARSET='utf-8'
 
-# -i Ignore case when searching
-# -N Display line numbers
-# -w Temporarily highlights the first "new" line after a forward movement of a full page.
-# -z-4 Changes the default scrolling window size of n lines. Negative value is n less than total lines.
-# -F Quit if entire file can be displayed on one screen (use -X with this)
-# -R Causes raw control characters to be displayed
-# -P Custom prompt
 # -g Highlight only the match on the particular string from by the last search command, not ALL strings.
-export LESS='-g -w -z-4 -R -P%t?f%f :stdin .?pb%pb\%:?lbLine %lb:?bbByte %bb:-...'
+# -w Temporarily highlights the first "new" line after a forward movement of a full page.
+# -R Causes raw control characters to be displayed
+# -P Custom prompt: (filename|stdin) position of the bottom line in file (in order of preference: % in file, or line #, or bytes
+#       ?f ... : ... . - if there is an input file. E.g. "?f%f :stdin ." will print filename followed by space, or "stdin "
+#       %f - current input file name
+#       ?pb - if % into file in terms of bytes is known using bottom line as the reference point
+#       %pb - % into file in bytes (vs lines) using bottom line as the reference point
+export LESS='-g -w -R -P?f%f :stdin .?pb%pb\%:?lbLine %lb:?bbByte %bb:-...'
+
