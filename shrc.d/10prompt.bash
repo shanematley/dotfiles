64f348a9bbe5319aca61f3380700d9a95b75a325
Shane <general@matley.com.au>
2021-02-11 18:46:04 +1100

    SH: Add (Docker) to prompt when in docker environment

--- shrc.d/10prompt.bash
+++ shrc.d/10prompt.bash
@@ -1,17 +1,12 @@
 #!/bin/bash
 # To enable VCS information enable via the following:
-#   ENABLE_VCS_PROMPT=1
 #   ENABLE_GIT_PROMPT=1
-#   ENABLE_HG_PROMPT=1
 #
 # (Note Git prompt will default to the builtin support if present.)
 #
 # To enable/disable vcs integration run the following at any time:
 #   enable_vcs_prompt
 #   disable_vcs_prompt
-
-[[ $OSTYPE =~ darwin* ]] && Apple=true
-[[ $OSTYPE =~ linux* ]] && Linux=true
 
 prompt_git() {
     git branch &>/dev/null || return 1
@@ -21,52 +16,42 @@ prompt_git() {
     #    grep -F 'working directory clean')" ]] || STATUS="!"
     # This appears much quicker though won't indicate untracked files
     git diff --quiet &>/dev/null || STATUS="!"
-    printf ' (git:%s)' "${BRANCH:-unknown}${STATUS}"
+    printf ' %s' "${BRANCH:-unknown}${STATUS}"
 }
-prompt_hg() {
-    hg branch &>/dev/null || return 1
-    BRANCH="$(hg branch 2>/dev/null)"
-    [[ -n "$(hg status 2>/dev/null)" ]] && STATUS="!"
-    printf ' (hg:%s)' "${BRANCH:-unknown}${STATUS}"
-}
-prompt_svn() {
-    svn info &>/dev/null || return 1
-    URL="$(svn info 2>/dev/null | \
-        awk -F': ' '$1 == "URL" {print $2}')"
-    ROOT="$(svn info 2>/dev/null | \
-        awk -F': ' '$1 == "Repository Root" {print $2}')"
-    BRANCH=${URL/$ROOT}
-    BRANCH=${BRANCH#/}
-    BRANCH=${BRANCH#branches/}
-    BRANCH=${BRANCH%%/*}
-    [[ -n "$(svn status 2>/dev/null)" ]] && STATUS="!"
-    printf ' (svn:%s)' "${BRANCH:-unknown}${STATUS}"
-}
-prompt_vcs() {
-    prompt_git
-}
+
 prompt_jobs() {
     [[ -n "$(jobs)" ]] && printf ' {%d}' $(jobs | sed -n '$=')
 }
 
+create_my_prompt() {
+    local VCS_PROMPT="$1"
+    local IN_DOCKER=""
+
+    [[ -f /.dockerenv ]] && IN_DOCKER="(Docker) "
+
+    export PS1="\[\033[34m\]\w\[\033[30m\]${VCS_PROMPT} \u@\h\$(prompt_jobs)\n\[\033[35m\]${IN_DOCKER}λ \[\033[0m\]"
+}
+
 enable_vcs_prompt() {
     # With built-in git completion support:
+    local VCS_PROMPT=
     if type -t __git_ps1 &>/dev/null; then
         export GIT_PS1_SHOWSTASHSTATE=1
         export GIT_PS1_SHOWDIRTYSTATE=1
         export GIT_PS1_SHOWUPSTREAM="git"
-        export PS1="[\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w]\[\033[00m\]\$(__git_ps1 ' (%s)')\$(prompt_jobs) "
+        create_my_prompt "\$(__git_ps1 ' (%s)')"
     else
-        export PS1="[\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w]\[\033[00m\]\$(prompt_vcs)\$(prompt_jobs) "
+        create_my_prompt "\$(prompt_git)"
     fi
 }
 
 disable_vcs_prompt() {
-    export PS1="\[\033[35m\]► \[\033[01;32m\]\u@\h\[\033[35m\]:\[\033[01;36m\]\w\[\033[0m\]\[\033[35m\]\$(prompt_jobs)\nλ\[\033[00m\] "
+    create_my_prompt ""
 }
 
+
 if [[ -n $PS1 ]]; then
-    if [[ $ENABLE_VCS_PROMPT == 1 || $ENABLE_GIT_PROMPT == 1 ]]; then
+    if [[ $ENABLE_GIT_PROMPT == 1 ]]; then
         enable_vcs_prompt
     else
         disable_vcs_prompt
--- shrc.d/20prompt.zsh
+++ shrc.d/20prompt.zsh
@@ -5,4 +5,5 @@ PURE_ALWAYS_SHOW_USERNAME=1
 PURE_DISPLAY_JOBS=1
 
 # Use pure prompt if available. It is a submodule of this repo.
+[[ -f /.dockerenv ]] && export PURE_PROMPT_SYMBOL="(Docker) ❯"
 prompt -l 2>/dev/null|grep -qw pure && prompt pure
