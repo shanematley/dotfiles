e09623d7d4f4f504abf2458c026ee58f57e65cc3
Shane <general@matley.com.au>
2021-03-25 10:16:26 +1100

    Replace zgen with submodules

--- shrc.d/90zshplugins.zsh
+++ shrc.d/90zshplugins.zsh
@@ -1,11 +1,25 @@
-#!/bin/zsh
+
+MY_LOCAL_PLUGINS="${0:a:h}/zshplugins"
+
+# TODO: add unixorn/git-extra-commands?
+# TODO: add RobSis/zsh-completion-generator
+
+source "${MY_LOCAL_PLUGINS}/k/k.sh"
+source "${MY_LOCAL_PLUGINS}/zsh-bd/bd.zsh"
+source "${MY_LOCAL_PLUGINS}/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+################################################################################
+# ZAW
+################################################################################
+
+source "${MY_LOCAL_PLUGINS}/zaw/zaw.zsh"
 
 bindkey '^R' zaw-history
 bindkey '^X\t' zaw-cdr
 bindkey '^X;' zaw
-bindkey '^Xa' zaw-searcher
+#bindkey '^Xa' zaw-searcher
 bindkey '\ec' zaw-cdr
-bindkey '\ea' zaw-searcher
+#bindkey '\ea' zaw-searcher
 bindkey '\er' zaw-fasd-files
 bindkey '\eg' zaw-git-files
 bindkey '\ee' zaw-fasd-directories
@@ -30,3 +44,9 @@ unset "zaw_sources[locate]"
 unset "zaw_sources[screens]"
 unset "zaw_sources[git-files-legacy]"
 
+################################################################################
+# zsh-syntax-highlighting
+################################################################################
+
+# Required to be the last thing sourced according to https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
+source "${MY_LOCAL_PLUGINS}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
