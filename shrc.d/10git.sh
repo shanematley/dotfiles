c36ef7b93aa57dee7512118bdffb0b4c750c55db
Shane Matley <shane@matley.com.au>
2022-03-08 11:26:50 +1100

    Tweak git commands

--- shrc.d/10git.sh
+++ shrc.d/10git.sh
@@ -4,7 +4,7 @@ function gs() {
     echo "$(tput bold)$(tput setaf 4)OUTGOING$(tput sgr0)"
     git -c color.status=always outgoing|sed 's/^/  /'
     echo "$(tput bold)$(tput setaf 4)RECENT$(tput sgr0)"
-    git --no-pager graph -10
+    git --no-pager lg --branches -10 --date-order
     echo
 }
 alias gitn="git --no-pager"
