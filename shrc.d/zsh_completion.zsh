ed1e4b2594b210c7311842bc78eb68c3f4dc6e7d
Shane <general@matley.com.au>
2022-08-18 22:46:04 +1000

    ZSH: Improve completions

--- shrc.d/zsh_completion.zsh
+++ shrc.d/zsh_completion.zsh
@@ -1,12 +1,22 @@
 #!/bin/zsh
 
+# For details on this, man zshoptions and man zshcompsys
+
 unsetopt menu_complete
 unsetopt flowcontrol
+# Automatically use menu completion after the second consecutive request for completion.
 setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
 setopt auto_name_dirs
 setopt globcomplete
+
+# Make cd push the old directory onto the directory stack
+setopt auto_pushd
+# Don't push multiple copies of the same directory onto the directory stack
+setopt pushd_ignore_dups
+# Don't print the directory stack after pushd or popd
+setopt pushd_silent
 
 
 autoload -U compinit
@@ -37,7 +47,6 @@ zstyle ':completion:*:*:*:*:*' menu select
 # "processes" is standard tag for process identifiers
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 # Use caching so that commands like apt and dpkg complete are useable
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "/tmp/.zsh_${USER}_cache"
@@ -45,6 +54,14 @@ zstyle ':completion:*' accept-exact '*(N)'
 # case-insensitive (all),partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
     'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Group different types of matches
+zstyle ':completion:*' group-name ''
+# Display a short description of the matches in a completion list
+zstyle ':completion:*:descriptions' format %d
+zstyle ':completion:*:descriptions' format %B%d%b
+# Set the order of groups of matches for cd and pushd
+zstyle ':completion:*:complete:(cd|pushd):*' tag-order 'local-directories named-directories path-directories'
 
 # Don't complete uninteresting users -- Andrey's list
 zstyle ':completion:*:*:*:users' ignored-patterns \
