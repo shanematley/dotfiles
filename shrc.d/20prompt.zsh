5943cbaee8c997c9612a610892be777ed5f2b301
Shane Matley <shane@matley.com.au>
2022-10-19 21:14:19 +1100

    ZSH: Only use pure prompt when TERM is set

--- shrc.d/20prompt.zsh
+++ shrc.d/20prompt.zsh
@@ -1,9 +1,11 @@
-autoload -U promptinit; promptinit
+if [[ -n $TERM ]]; then
+    autoload -U promptinit; promptinit
 
-PURE_GIT_UNTRACKED_DIRTY=0
-PURE_ALWAYS_SHOW_USERNAME=1
-PURE_DISPLAY_JOBS=1
+    PURE_GIT_UNTRACKED_DIRTY=0
+    PURE_ALWAYS_SHOW_USERNAME=1
+    PURE_DISPLAY_JOBS=1
 
-# Use pure prompt if available. It is a submodule of this repo.
-[[ -f /.dockerenv ]] && export PURE_PROMPT_SYMBOL="(Docker) ❯"
-prompt -l 2>/dev/null|grep -qw pure && prompt pure
+    # Use pure prompt if available. It is a submodule of this repo.
+    [[ -f /.dockerenv ]] && export PURE_PROMPT_SYMBOL="(Docker) ❯"
+    prompt -l 2>/dev/null|grep -qw pure && prompt pure
+fi
