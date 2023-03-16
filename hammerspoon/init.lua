0cc33b7475942413e71ea32fd6453779a09802d9
Shane Matley <shane@matley.com.au>
2023-03-17 09:28:29 +1100

    Hammerspoon: Add better "hiding" of Zoom sharing controls
    The better "hiding" is acheived through moving the screen share controls
    outside the bounds of the screen, and then moving them back when
    requested.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -11,3 +11,21 @@ spoon.SendToOmniFocus:bindHotkeys({
     send_to_omnifocus = { {"shift", "ctrl", "alt", "cmd" }, "t" }
 })
 
+local originalFrame = nil
+
+hs.hotkey.bind({"cmd", "ctrl", "alt"}, "H", function()
+  local zoomWindow = hs.window.find("zoom share statusbar window")
+  if zoomWindow then
+    if originalFrame then
+      zoomWindow:setFrame(originalFrame)
+      originalFrame = nil
+    else
+      originalFrame = zoomWindow:frame()
+      local screen = zoomWindow:screen()
+      local frame = zoomWindow:frame()
+      frame.x = screen:frame().w + 3000
+      frame.y = screen:frame().h + 3000
+      zoomWindow:setFrame(frame)
+    end
+  end
+end)
