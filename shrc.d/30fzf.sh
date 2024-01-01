0225fa737057998e55388d2e18e314fd201050ec
Shane <general@matley.com.au>
2024-01-01 18:05:30 +1000

    Add ability to change color themes via dark/light functions
    Changes the themes on:
    
    - Alacritty
    - VIM
    - FZF
    - Bat

--- shrc.d/30fzf.sh
+++ shrc.d/30fzf.sh
@@ -7,7 +7,7 @@ if command -v highlight >/dev/null 2>&1; then
 
     export FZF_CTRL_T_OPTS="
         --multi
-        --preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'
+        --preview '(bat -n --color=always {} || cat {} | tree -C {}) 2> /dev/null | head -200'
         --bind 'alt-enter:become(vim {+} < /dev/tty > /dev/tty),ctrl-y:execute-silent(echo -n {+} | pbcopy)+abort,ctrl-/:change-preview-window(50%|hidden|)'
         --color header:italic
         --header 'Press CTRL-Y to copy file paths into clipboard. ALT-Enter to open in vim. Ctrl-/ (Ctrl-_) to toggle preview'"
@@ -15,7 +15,22 @@ fi
 
 # Use tmux by default. This is ignored when not in tmux
 export FZF_TMUX_OPTS='-p80%,60%'
-export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'"
+export FZF_DEFAULT_OPTS_BASE="--height=40% --layout=reverse --info=inline --border"
+
+fzf_set_dark_color_scheme() {
+    export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS_BASE} --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'"
+}
+
+fzf_set_light_color_scheme() {
+    export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS_BASE}
+        --color=fg:#4d4d4c,bg:#eeeeee,hl:#d7005f
+        --color=fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f
+        --color=info:#4271ae,prompt:#8959a8,pointer:#d7005f
+        --color=marker:#4271ae,spinner:#4271ae,header:#4271ae"
+}
+
+# Note: the fzf color scheme is set in 99colorscheme.sh
+
 export FZF_CTRL_R_OPTS="
     --sort
     --preview 'echo {}' --preview-window down:5:wrap --bind 'ctrl-/:toggle-preview'
