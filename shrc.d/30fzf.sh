d5cddf685a3bf13791afe93a855837435fdc1560
Shane Matley <shane@matley.com.au>
2024-01-02 13:19:05 +1100

    FZF: Update keybindings and functionality to work on Linux and SSH

--- shrc.d/30fzf.sh
+++ shrc.d/30fzf.sh
@@ -1,17 +1,35 @@
 
-if command -v highlight >/dev/null 2>&1; then
+if command -v bat >/dev/null 2>&1; then
     fif() {
         if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
-        rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+        rg --files-with-matches --no-messages "$1" | \
+            fzf --preview "bat -n --color=always {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}" --bind 'alt-enter:become(vim {+} < /dev/tty > /dev/tty)'
+    }
+fi
+
+if command -v fd >/dev/null 2>&1; then
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
     }
 
-    export FZF_CTRL_T_OPTS="
-        --multi
-        --preview '(bat -n --color=always {} || cat {} | tree -C {}) 2> /dev/null | head -200'
-        --bind 'alt-enter:become(vim {+} < /dev/tty > /dev/tty),ctrl-y:execute-silent(echo -n {+} | pbcopy)+abort,ctrl-/:change-preview-window(50%|hidden|)'
-        --color header:italic
-        --header 'Press CTRL-Y to copy file paths into clipboard. ALT-Enter to open in vim. Ctrl-/ (Ctrl-_) to toggle preview'"
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+
+    export FZF_CTRL_T_COMMAND="fd --hidden --follow --exclude .git"
 fi
+
+export FZF_CTRL_T_OPTS="
+    --multi
+    --preview '(bat -n --color=always {} || cat {} | tree -C {}) 2> /dev/null | head -200'
+    --bind 'alt-enter:become(vim {+} < /dev/tty > /dev/tty),ctrl-y:execute-silent(echo -n {+} | yank.sh > /dev/tty)+abort,ctrl-/:change-preview-window(50%|hidden|),ctrl-w:reload(fd --hidden --follow --exclude .git .),ctrl-e:reload(fd --hidden --follow --exclude .git -I .)'
+    --color header:italic
+    --header 'C-Y: copy to clipboard. A-Enter: vim. C-/ (C-_): toggle preview. C-w: no ignored. C-e: show ignored'"
 
 # Use tmux by default. This is ignored when not in tmux
 export FZF_TMUX_OPTS='-p80%,60%'
@@ -33,8 +51,8 @@ fzf_set_light_color_scheme() {
 
 export FZF_CTRL_R_OPTS="
     --sort
-    --preview 'echo {}' --preview-window down:5:wrap --bind 'ctrl-/:toggle-preview'
-    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort,ctrl-/:change-preview-window(50%|hidden|)'
+    --preview 'echo {}' --preview-window down:5:wrap
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | yank.sh > /dev/tty)+abort,ctrl-/:change-preview-window(50%|hidden|)'
     --header 'Press CTRL-Y to copy commands into clipboard. Ctrl-/ (Ctrl-_) to toggle preview 50%'
     --color header:italic"
 
@@ -42,11 +60,9 @@ export FZF_CTRL_R_OPTS="
 export FZF_ALT_C_OPTS="
     --preview 'tree -C {} | head -200'
     --header 'Press CTRL-Y to copy commands into clipboard. Ctrl-/ (Ctrl-_) to toggle preview'
-    --bind='ctrl-y:execute-silent(echo -n {+} | pbcopy)+abort,ctrl-/:change-preview-window(hidden|)'"
+    --bind='ctrl-y:execute-silent(echo -n {+} | yank.sh > /dev/tty)+abort,ctrl-/:change-preview-window(hidden|)'"
 
-# From https://github.com/junegunn/fzf#settings
-#
-# (EXPERIMENTAL) Advanced customization of fzf options via _fzf_comprun function
+# Advanced customization of fzf options via _fzf_comprun function
 # - The first argument to the function is the name of the command.
 # - You should make sure to pass the rest of the arguments to fzf.
 _fzf_comprun() {
@@ -54,37 +70,37 @@ _fzf_comprun() {
   shift
 
   case "$command" in
-    cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
-    export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
-    ssh)          fzf "$@" --preview 'dig {}' ;;
-    *)            fzf "$@" ;;
+    cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
+    export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
   esac
 }
 
+
 if command -v kubectl >/dev/null 2>&1; then
     pods() {
-    FZF_DEFAULT_COMMAND="kubectl get pods --all-namespaces" \
-        fzf --info=inline --layout=reverse --header-lines=1 \
-            --prompt "$(kubectl config current-context | sed 's/-context$//')> " \
-            --header $'╱ Enter (kubectl exec) ╱ CTRL-O (open log in editor) ╱ CTRL-R (reload) ╱\n\n' \
-            --bind 'ctrl-/:change-preview-window(80%,border-bottom|hidden|)' \
-            --bind 'enter:execute:kubectl exec -it --namespace {1} {2} -- bash > /dev/tty' \
-            --bind 'ctrl-o:execute:${EDITOR:-vim} <(kubectl logs --all-containers --namespace {1} {2}) > /dev/tty' \
-            --bind 'ctrl-r:reload:$FZF_DEFAULT_COMMAND' \
-            --preview-window up:follow \
-            --preview 'kubectl logs --follow --all-containers --tail=10000 --namespace {1} {2}' "$@"
+        FZF_DEFAULT_COMMAND="kubectl get pods --all-namespaces" \
+            fzf-tmux --info=inline --layout=reverse --header-lines=1 \
+                --prompt "$(kubectl config current-context | sed 's/-context$//')> " \
+                --header $'╱ Enter (kubectl exec) ╱ CTRL-O (open log in editor) ╱ CTRL-R (reload) ╱\n\n' \
+                --bind 'ctrl-/:change-preview-window(80%,border-bottom|hidden|)' \
+                --bind 'enter:execute:kubectl exec -it --namespace {1} {2} -- bash > /dev/tty' \
+                --bind 'ctrl-o:execute:${EDITOR:-vim} <(kubectl logs --all-containers --namespace {1} {2}) > /dev/tty </dev/tty' \
+                --bind 'ctrl-r:reload:$FZF_DEFAULT_COMMAND' \
+                --preview-window up:follow \
+                --preview 'kubectl logs --follow --all-containers --tail=10000 --namespace {1} {2}' "$@"
     }
 fi
 
 fzf_diff() {
-        preview="git diff $@ --color=always -- {-1}"
-        git diff $@ --name-only | fzf -m --ansi --preview $preview --preview-window wrap
-}
-
-fzf_diff() {
+    local preview="git diff $@ --color=always -- {-1}"
+    if command -v bat >/dev/null 2>&1; then
         preview="git diff --name-only --diff-filter=d --relative -- {-1} | \
             xargs bat --diff --color=always --style=numbers,changes {-1}"
-        git diff $@ --name-only | fzf -m --ansi --preview $preview
+    fi
+    git diff $@ --name-only | \
+        fzf -m --ansi --preview $preview --bind 'alt-enter:become(vim {+} >/dev/tty </dev/tty)'
 }
 
 # Interactive jq queries using FZF
