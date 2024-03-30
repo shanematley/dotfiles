493ae9d9d6d90ca617e0222370df143fbe097b41
Shane Matley <shane@matley.com.au>
2024-03-30 16:42:56 +1100

    Produce Markdown without metadata from extract-subtitles

--- shrc.d/50subtitles.sh
+++ shrc.d/50subtitles.sh
@@ -1,8 +1,13 @@
 function extract-subtitles() {
     local src
-    local dst
+    local dst_srt
+    local dst_md
     src="$1"
-    dst="${src}.srt"
+    dst_srt="${src}.srt"
+    dst_md="${src}.md"
 
-    ffmpeg -i "$src" -map 0:s:0 -codec:s text "$dst"
+    ffmpeg -i "$src" -map 0:s:0 -codec:s text "$dst_srt"
+    # Remote timestamps and place in a different file
+    echo -n "# ${src} Subtitles\n\nExtracted: $(date)\n\n" > "$dst_md"
+    perl -0007pe 's/^\n*\d+\n[\d:,]+ --> [\d:,]+\n//gm' "$dst_srt" >> "$dst_md"
 }
