9cd89ed90870a82446babd6a491b3d42c6624b22
Shane Matley <shane@matley.com.au>
2025-02-28 09:52:59 +0100

    Git: update defaults

--- shrc.d/30fzf.sh
+++ shrc.d/30fzf.sh
@@ -21,12 +21,12 @@ if command -v fd >/dev/null 2>&1; then
     # - The first argument to the function ($1) is the base path to start traversal
     # - See the source code (completion.{bash,zsh}) for the details.
     _fzf_compgen_path() {
-        fd --hidden --follow --exclude ".git" --strip-cwd-prefix . "$1"
+        fd --hidden --follow --exclude ".git"  . "$1"
     }
 
     # Use fd to generate the list for directory completion
     _fzf_compgen_dir() {
-        fd --type d --hidden --follow --exclude ".git" --strip-cwd-prefix . "$1"
+        fd --type d --hidden --follow --exclude ".git" . "$1"
     }
 
     export FZF_CTRL_T_COMMAND="fd --hidden --follow --exclude .git --strip-cwd-prefix"
